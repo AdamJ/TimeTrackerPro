d0cff87d5136d7d7fa419c99e1f089f901f7e59c
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2026-06-10 07:28:25 -0400

    fix: begin project sheet work

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-17772b39'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9n3dggvo1sg"
+    "revision": "0.ecat07kqfug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
